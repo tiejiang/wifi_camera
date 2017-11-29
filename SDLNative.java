f38e9ee346d019040547f5253c5b570de10b9ca0
tiejiang <315904145@qq.com>
2017-11-30 00:48:37 +0800

    first init this smart glasses project use wifi camera sdk

---
+++ SDLNative.java
@@ -0,0 +1,176 @@
+package com.intchip.media;
+
+import java.util.Arrays;
+
+
+import android.annotation.TargetApi;
+import android.media.AudioFormat;
+import android.media.AudioTrack;
+import android.os.Build;
+import android.util.Log;
+import android.view.InputDevice;
+import android.view.Surface;
+import android.view.SurfaceView;
+
+public class SDLNative {
+	public static final String TAG = "SDLNative";
+	
+	private static SurfaceView mSurfaceView;
+	protected static AudioTrack mAudioTrack;
+	public static int screenWidth, screenHeight;
+	
+	public SDLNative(SurfaceView sv) {
+		mSurfaceView = sv;
+	}
+	
+	public SDLNative(SurfaceView sv, int w, int h) {
+		mSurfaceView = sv;
+		screenWidth = w;
+		screenHeight = h;
+	}
+	
+	public static void setScreeen(int w, int h) {
+		screenWidth = w;
+		screenHeight = h;
+	}
+
+	public static native int nativeInit(SDLNative object);
+	
+	public static native void onNativeResize(int x, int y, int format);
+	
+	public static native void nativeQuit();
+	
+	public static native void nativePause();
+	
+	public static native void nativeResume();
+	
+	public static native void onNativeSurfaceChanged();
+	
+    public static native void onNativeSurfaceDestroyed();
+    
+    public static native void nativeLowMemory();
+    
+    public static native void nativeFlipBuffers();
+    
+    public native static void screenChange(int screenWidth, int screenHeight);
+    
+    public native static int quitVideoPlay();
+    
+    public native static int startVodPlay();
+    
+    public native static int stopVodPlay();
+    
+    public native static int snapshot(String filename);
+    
+    public native static int reversal();
+    
+    public native static int videoRecordStart(String filename);
+    public native static int videoRecordStop();
+    public native static int isVideoRecord();
+    
+    public static Surface getNativeSurface() {
+        return mSurfaceView.getHolder().getSurface();
+    }
+    
+    public static void flipBuffers() {
+        nativeFlipBuffers();
+    }
+        
+ // Audio
+    public static int audioInit(int sampleRate, boolean is16Bit, boolean isStereo, int desiredFrames) {
+        int channelConfig = isStereo ? AudioFormat.CHANNEL_CONFIGURATION_STEREO : AudioFormat.CHANNEL_CONFIGURATION_MONO;
+        int audioFormat = is16Bit ? AudioFormat.ENCODING_PCM_16BIT : AudioFormat.ENCODING_PCM_8BIT;
+        int frameSize = (isStereo ? 2 : 1) * (is16Bit ? 2 : 1);
+        
+        Log.d(TAG, "SDL audio: wanted " + (isStereo ? "stereo" : "mono") + " " + (is16Bit ? "16-bit" : "8-bit") + " " + (sampleRate / 1000f) + "kHz, " + desiredFrames + " frames buffer");
+        
+        // Let the user pick a larger buffer if they really want -- but ye
+        // gods they probably shouldn't, the minimums are horrifyingly high
+        // latency already
+        desiredFrames = Math.max(desiredFrames, (AudioTrack.getMinBufferSize(sampleRate, channelConfig, audioFormat) + frameSize - 1) / frameSize);
+        return 0;
+    }
+    
+    public static void audioWriteShortBuffer(short[] buffer) {
+        for (int i = 0; i < buffer.length; ) {
+			int result = 0;
+			if (null != mAudioTrack)
+            	result = mAudioTrack.write(buffer, i, buffer.length - i);
+            if (result > 0) {
+                i += result;
+            } else if (result == 0) {
+                try {
+                    Thread.sleep(1);
+                } catch(InterruptedException e) {
+                }
+            } else {
+                Log.d(TAG, "SDL audio: error return from write(short)");
+                return;
+            }
+        }
+    }
+    
+    public static void audioWriteByteBuffer(byte[] buffer) {
+        for (int i = 0; i < buffer.length; ) {
+			int result = 0;
+			if (null != mAudioTrack)
+            	result = mAudioTrack.write(buffer, i, buffer.length - i);
+            if (result > 0) {
+                i += result;
+            } else if (result == 0) {
+                try {
+                    Thread.sleep(1);
+                } catch(InterruptedException e) {
+                    // Nom nom
+                }
+            } else {
+                Log.w("SDL", "SDL audio: error return from write(short)");
+                return;
+            }
+        }
+    }
+    
+    public static void audioQuit() {
+        if (mAudioTrack != null) {
+            mAudioTrack.stop();
+            mAudioTrack = null;
+        }
+    }
+    
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+	/**
+     * @return an array which may be empty but is never null.
+     */
+	public static int[] inputGetInputDeviceIds(int sources) {
+        int[] ids = InputDevice.getDeviceIds();
+        int[] filtered = new int[ids.length];
+        int used = 0;
+        for (int i = 0; i < ids.length; ++i) {
+            InputDevice device = InputDevice.getDevice(ids[i]);
+            if ((device != null) && ((device.getSources() & sources) != 0)) {
+                filtered[used++] = device.getId();
+           }
+        }
+        return Arrays.copyOf(filtered, used);
+    }
+    
+    public static void pollInputDevices() {
+    	
+    }
+    
+    public static boolean setActivityTitle(String title) {
+        return true;
+    }
+    
+    static {
+    	System.loadLibrary("SDL");
+    	System.loadLibrary("swresample-2");
+    	System.loadLibrary("swscale-4");
+    	System.loadLibrary("avformat-57");
+    	System.loadLibrary("avutil-55");
+    	System.loadLibrary("avcodec-57");
+    	System.loadLibrary("mp4v2");
+    	System.loadLibrary("mzmedia");
+    }
+
+}
---
+++ StreamMadiaPlayer.java
@@ -0,0 +1,283 @@
+package com.intchip.efdrc;
+
+import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import com.intchip.efdrc.R;
+import com.intchip.media.SDLNative;
+
+public class StreamMadiaPlayer implements SurfaceHolder.Callback {
+	private final String TAG = "StreamMadiaPlayer";
+
+	private SurfaceHolder surfaceHolder;
+
+	public static boolean mIsPaused = false, mIsSurfaceReady = false,
+			mHasFocus = true;
+	public static boolean mExitCalledFromJava;
+
+	private static SurfaceView mSurface;
+
+	// This is what SDL runs in. It invokes SDL_main(), eventually
+	protected static Thread mSDLThread;
+
+	protected static Activity mSingleton;
+
+	public static int screenWidth, screenHeight;
+
+	public StreamMadiaPlayer(Activity mainActivity) {
+		mSingleton = mainActivity;
+
+		mSurface = (SurfaceView) mainActivity.findViewById(R.id.main_surface);
+		surfaceHolder = mSurface.getHolder();
+		surfaceHolder.addCallback(this);
+
+		initialize();
+
+		getScreenResolution();
+	}
+
+	public static void initialize() {
+		mSDLThread = null;
+		mExitCalledFromJava = false;
+		mIsPaused = false;
+		mIsSurfaceReady = false;
+		mHasFocus = true;
+	}
+
+	public void onLowMemory() {
+		Log.d(TAG, "onLowMemory()");
+		SDLNative.nativeLowMemory();
+	}
+
+	public void onWindowFocusChanged(boolean hasFocus) {
+		Log.d(TAG, "onWindowFocusChanged(): " + hasFocus);
+
+//		Toast.makeText(mSingleton, "onWindowFocusChanged() ", Toast.LENGTH_SHORT).show();
+		
+		mHasFocus = hasFocus;
+		if (hasFocus) {
+			handleResume();
+		}
+	}
+
+	public void onResume() {
+		Log.d(TAG, "onResume()");
+		
+//		Toast.makeText(mSingleton, "onResume() ", Toast.LENGTH_SHORT).show();
+		
+		handleResume();
+	}
+
+	public static void handleResume() {
+		if (mIsPaused && mIsSurfaceReady && mHasFocus) {
+			mIsPaused = false;
+			SDLNative.nativeResume();
+		}
+	}
+
+	public void onPause() {
+		Log.d(TAG, "onPause()");
+		handlePause();
+//		Toast.makeText(mSingleton, "onPause", Toast.LENGTH_SHORT).show();
+	}
+
+	/**
+	 * Called by onPause or surfaceDestroyed. Even if surfaceDestroyed is the
+	 * first to be called, mIsSurfaceReady should still be set to 'true' during
+	 * the call to onPause (in a usual scenario).
+	 */
+	public static void handlePause() {
+		if (!mIsPaused && mIsSurfaceReady) {
+			mIsPaused = true;
+			SDLNative.nativePause();
+		}
+	}
+
+	public void onDestory() {
+		Log.d(TAG, "onDestory");
+		
+//		Toast.makeText(mSingleton, "onDestory", Toast.LENGTH_SHORT).show();
+		
+		SDLNative.nativeQuit();
+
+		mExitCalledFromJava = true;
+
+		SDLNative.quitVideoPlay();
+
+		// Now wait for the SDL thread to quit
+		if (mSDLThread != null) {
+			try {
+				mSDLThread.join();
+			} catch (Exception e) {
+				Log.d(TAG, "Problem stopping thread: " + e);
+			}
+			mSDLThread = null;
+
+			Log.d(TAG, "Finished waiting for SDL thread");
+		}
+
+		initialize();
+	}
+	
+	public void onNetworkConnected() {
+		Log.d(TAG, "onNetworkConnected()");
+		
+		new Thread(new SDLVod(true), "SDLVodThread start").start();
+		
+//		Toast.makeText(mSingleton, "onNetworkConnected", Toast.LENGTH_SHORT).show();
+	}
+	
+	public void onNetworkDisconnected() {
+		Log.d(TAG, "onNetworkDisconnected()");
+		
+		new Thread(new SDLVod(false), "SDLVodThread stop").start();
+		
+//		Toast.makeText(mSingleton, "onNetworkDisconnected", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		Log.d(TAG, "surfaceCreated()");
+		holder.setType(SurfaceHolder.SURFACE_TYPE_GPU);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		Log.d(TAG, "surfaceChanged");
+
+//		Toast.makeText(mSingleton, "surfaceChanged", Toast.LENGTH_SHORT).show();
+		
+		int sdlFormat = 0x15151002; // SDL_PIXELFORMAT_RGB565 by default
+		switch (format) {
+		case PixelFormat.A_8:
+			Log.v("SDL", "pixel format A_8");
+			break;
+		case PixelFormat.LA_88:
+			Log.v("SDL", "pixel format LA_88");
+			break;
+		case PixelFormat.L_8:
+			Log.v("SDL", "pixel format L_8");
+			break;
+		case PixelFormat.RGBA_4444:
+			Log.v("SDL", "pixel format RGBA_4444");
+			sdlFormat = 0x15421002; // SDL_PIXELFORMAT_RGBA4444
+			break;
+		case PixelFormat.RGBA_5551:
+			Log.v("SDL", "pixel format RGBA_5551");
+			sdlFormat = 0x15441002; // SDL_PIXELFORMAT_RGBA5551
+			break;
+		case PixelFormat.RGBA_8888:
+			Log.v("SDL", "pixel format RGBA_8888");
+			sdlFormat = 0x16462004; // SDL_PIXELFORMAT_RGBA8888
+			break;
+		case PixelFormat.RGBX_8888:
+			Log.v("SDL", "pixel format RGBX_8888");
+			sdlFormat = 0x16261804; // SDL_PIXELFORMAT_RGBX8888
+			break;
+		case PixelFormat.RGB_332:
+			Log.v("SDL", "pixel format RGB_332");
+			sdlFormat = 0x14110801; // SDL_PIXELFORMAT_RGB332
+			break;
+		case PixelFormat.RGB_565:
+			Log.v("SDL", "pixel format RGB_565");
+			sdlFormat = 0x15151002; // SDL_PIXELFORMAT_RGB565
+			break;
+		case PixelFormat.RGB_888:
+			Log.v("SDL", "pixel format RGB_888");
+			// Not sure this is right, maybe SDL_PIXELFORMAT_RGB24 instead?
+			sdlFormat = 0x16161804; // SDL_PIXELFORMAT_RGB888
+			break;
+		default:
+			Log.v("SDL", "pixel format unknown " + format);
+			break;
+		}
+
+		Log.d(TAG, "Window size:" + width + "x" + height);
+		SDLNative.onNativeResize(width, height, sdlFormat);
+
+		// Set mIsSurfaceReady to 'true' *before* making a call to handleResume
+		mIsSurfaceReady = true;
+		SDLNative.onNativeSurfaceChanged();
+
+		if (mSDLThread == null) {
+			mSDLThread = new Thread(new SDLMain(), "SDLThread");
+			mSDLThread.start();
+
+			// Set up a listener thread to catch when the native thread ends
+			new Thread(new Runnable() {
+				public void run() {
+					try {
+						mSDLThread.join();
+					} catch (Exception e) {
+					} finally {
+						// Native thread has finished
+						if (!mExitCalledFromJava) {
+							handleNativeExit();
+						}
+					}
+				}
+			}).start();
+		}
+
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		Log.d(TAG, "surfaceDestroyed()");
+
+		handlePause();
+		// SDLNative.nativeQuit();
+
+		mIsSurfaceReady = false;
+		SDLNative.onNativeSurfaceDestroyed();
+	}
+
+	/**
+	 * Simple nativeInit() runnable
+	 */
+	class SDLMain implements Runnable {
+		SDLNative objSDLInfo = new SDLNative(mSurface, screenWidth, screenHeight);
+
+		public void run() {
+			SDLNative.nativeInit(objSDLInfo);
+		}
+	}
+	
+	class SDLVod implements Runnable {
+		private boolean startFlag = false;
+		
+		public SDLVod (boolean flag) {
+			startFlag = flag;
+        }		
+
+		public void run() {
+            if (startFlag) {
+            	int ret = SDLNative.startVodPlay();
+    			if (ret < 0) {
+    				Log.d(TAG, "startVodPlay failed");
+    			}
+            } else {
+            	SDLNative.stopVodPlay();
+            }
+		}
+	}
+
+	/* The native thread has finished */
+	public static void handleNativeExit() {
+		mSDLThread = null;
+		mSingleton.finish();
+	}
+
+	public void getScreenResolution() {
+		screenWidth = mSingleton.getWindowManager().getDefaultDisplay()
+				.getWidth();
+		screenHeight = mSingleton.getWindowManager().getDefaultDisplay()
+				.getHeight();
+
+	}
+}
